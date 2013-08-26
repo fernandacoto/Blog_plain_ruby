164d328f7f0c4b561d001afe9bbbd6adb2761872
fernandacoto <fermarioro@gmail.com>
2013-08-26 11:16:11 -0600

    write a comment

---
+++ spec/blog/blog_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+module Blog
+  describe Blog do
+    describe "#post" do
+      it "waits for title and a comment"
+    end
+    describe "#see#" do
+      it "should show all posts"
+    end
+    describe "#edit#" do
+      it "should update a post"
+    end
+    describe "#delete#" do
+      it "should delete a post"
+    end
+  end
+end
+
